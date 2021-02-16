3876abc9d4c8647a6bcebb0aca614045e89948a4
Mirsho <mirsho@protonmail.com>
2021-02-16 20:18:49 +0000

    Refactorización de funciones y peticiones.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,24 @@ const breedURL = 'https://dog.ceo/api/breeds/list/all';
 const photoURL1 = 'https://dog.ceo/api/breed/';
 const photoURL2 = '/images/random';
 
+const dogs = document.getElementById('doglist');
+
+let radios = document.getElementById('tipoconexion').getElementsByTagName('input');
+let connectionType = null;
+for (let i = 0; i < radios.length; i++) {
+  radios[i].addEventListener('change', function () {
+    if (this !== connectionType) {
+      connectionType = this;
+      switchConnection(connectionType.value);
+    }
+    console.log(this.value)
+    //Mostramos la sección de la lista de razas y el botón de nuevo perro.
+    let x = document.getElementById("perros");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    }
+  });
+}
 
 document.addEventListener("DOMContentLoaded", function (event) {
   console.log(event);
@@ -13,42 +31,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
   }
 });
 
-let radios = document.getElementById('tipoconexion').getElementsByTagName('input');
-let connectionType = null;
-for (let i = 0; i < radios.length; i++) {
-  radios[i].addEventListener('change', function () {
-    //(connectionType) ? console.log(connectionType.value) : null;
-    if (this !== connectionType) {
-      connectionType = this;
-      selectConnection(connectionType.value);
-    }
-    console.log(this.value)
-    //Mostramos la sección de la lista de razas y el botón de nuevo perro.
-    let x = document.getElementById("perros");
-    if (x.style.display === "none") {
-      x.style.display = "flex";
-    }
-  });
-}
-
-//!Crear event listeners para las opciones del select (la lista de perros)
-let newDog = document.getElementById('newdog');
-newDog.addEventListener('click', () => dogPhoto(dogs.value));
-
-//!REVISAR
-let selectedDog = document.getElementById('dogs');
-selectedDog.addEventListener('change', () => dogPhoto(dogs.value));
 /**
  * Ejecuta las llamadas a las funciones de la conexión seleccionada.
  * @param {String} type - Tipo de conexión marcada en el formulario de botones radio.
  */
-function selectConnection(type) {
+function switchConnection(type) {
+  clearNodes(document.getElementById('fotoperro'));
   switch (type) {
-    case "xml": clearNodes(document.getElementById('fotoperro'));
-      conexionXML(breedURL);
+    case "xml": conexionXML(breedURL);
       break;
     case "fetch": {
-      clearNodes(document.getElementById('fotoperro'));
       peticionFetch(breedURL)
         .then(response => JSON.parse(response))
         .then(data => {
@@ -59,9 +51,34 @@ function selectConnection(type) {
         .catch(error => console.error(error));
       break;
     }
-    case "jquery": clearNodes(document.getElementById('fotoperro'));
-      var jsonJQuery = peticionJQuery(breedURL);
-      console.log(jsonJQuery);
+    case "jquery": peticionJQuery(breedURL, 'list');
+      break;
+    default: console.log('Seleccione un tipo de conexión.');
+  }
+}
+
+let selectedDog = document.getElementById('doglist');
+selectedDog.addEventListener('change', () => changeBreed());
+
+let newDog = document.getElementById('newdog');
+newDog.addEventListener('click', () => changeBreed());
+
+function changeBreed() {
+  let photoURL = photoURL1 + dogs.value + photoURL2;
+  switch (connectionType.value) {
+    case "xml": xmlPhoto(photoURL);
+      break;
+    case "fetch": {
+      peticionFetch(photoURL)
+        .then(response => JSON.parse(response))
+        .then(data => {
+          dogPhoto(data);
+        })
+        .finally(() => console.log("Terminado."))
+        .catch(error => console.error(error));
+      break;
+    }
+    case "jquery": peticionJQuery(photoURL, 'photo');
       break;
     default: console.log('Seleccione un tipo de conexión.');
   }
@@ -87,10 +104,9 @@ function conexionXML(url) {
       if (dogexion.status == 200) {
         let dogBreeds = JSON.parse(dogexion.responseText);
         let breeds = Object.keys(dogBreeds.message);
-        console.log(breeds);
-        let firstDog = breeds[0]; //Asignar el atributo selected
         breeds.forEach(dog => listarPerros(dog));
-        dogPhoto(firstDog);
+        let dog = breeds[0];
+        xmlPhoto(photoURL1 + dog + photoURL2);
       }
       else {
         //!Ocultar spinner si lo hubiera
@@ -107,11 +123,11 @@ function conexionXML(url) {
  *
  * @param {*} dog
  */
-function dogPhoto(dog) {
+function xmlPhoto(url) {
   clearNodes(document.getElementById('fotoperro'));
   let dogexion = new XMLHttpRequest();
   dogexion.onreadystatechange = procesarEventos;
-  dogexion.open('GET', photoURL1 + dog + photoURL2, true);
+  dogexion.open('GET', url, true);
   dogexion.send();
 
   /**
@@ -122,9 +138,7 @@ function dogPhoto(dog) {
     if (dogexion.readyState == 4) {
       if (dogexion.status == 200) {
         let fotoPerro = JSON.parse(dogexion.responseText);
-        console.log(fotoPerro.message);
-        let foto = crearNodo("img", null, null, [], [{ name: 'src', value: fotoPerro.message }, { name: 'alt', value: dog }]);
-        console.log(foto);
+        let foto = crearNodo("img", null, null, [], [{ name: 'src', value: fotoPerro.message }, { name: 'alt', value: dogs.value }]);
         document.getElementById('fotoperro').appendChild(foto);
       }
       else {
@@ -141,21 +155,37 @@ function dogPhoto(dog) {
  * JQUERY - Conexión con API mediante Jquery
  * @param {String} path - URL de la API a la que nos queremos conectar mediante el métod JQuery
  */
-function peticionJQuery(path) {
+function peticionJQuery(path, resourceType) {
   $.ajax({
     url: path, //URL de la petición
     data: {}, //información a enviar, puede ser una cadena
     type: 'GET', //tipo de la petición: POST o GET
     dataType: 'json', //tipo de dato que se espera
     success: function (json) { //función a ejecutar si es satisfactoria 
-      let breeds = Object.keys(json.message);
-      breeds.forEach(dog => listarPerros(dog));
+      if (resourceType == 'list') {
+        getBreeds(json);
+      } else if (resourceType == 'photo') {
+        dogPhoto(json);
+      }
     }, error: function (jqXHR, status, error) { //función error 
       alert('Disculpe, existió un problema. Vuelva a intentarlo.');
-    }, finally: function(){},
+    }, finally: function () { },
     // función a ejecutar sin importar si la petición falló o no 
     complete: function (jqXHR, status) { console.log('Petición realizada'); }
   });
+}
+
+function getBreeds(data) {
+  let breeds = Object.keys(data.message);
+  breeds.forEach(dog => listarPerros(dog));
+  let firstDog = breeds[0];
+  peticionJQuery(photoURL1 + firstDog + photoURL2, 'photo');
+}
+
+function dogPhoto(data) {
+  clearNodes(document.getElementById('fotoperro'));
+  let foto = crearNodo("img", null, null, [], [{ name: 'src', value: data.message }, { name: 'alt', value: dogs.value }]);
+  document.getElementById('fotoperro').appendChild(foto);
 }
 
 /**
@@ -219,9 +249,6 @@ function crearNodo(tagName, nodeText, nodeId, nodeClasses, nodeAttributes) {
   return nodeElement;
 }
 
-const dogs = document.getElementById('dogs');
-console.log(dogs);
-
 /**
  * Crea y añade nodos al documento HTML en base a los datos del objeto recibido, ya sea utilizando HTML templates o la función crearNodo(), 
  * cuando el navegador no sea compatible con las templates.
@@ -235,13 +262,13 @@ function listarPerros(dog) {
   if ('content' in document.createElement('template')) {
     // Instanciar la lista
     // y su contenido con el template
-    var perro = document.querySelector('#doglist'),
+    var perro = document.querySelector('#dogoption'),
       opcion = perro.content.querySelectorAll("option");
-    opcion[0].setAttribute("Value", dog)
+    opcion[0].setAttribute("value", dog);
     opcion[0].textContent = dog;
 
     // Clonar el nuevo perro e insertarlo en la lista
-    var listaPerros = document.querySelector("#dogs");
+    var listaPerros = document.querySelector("#doglist");
     var clone = document.importNode(perro.content, true);
     listaPerros.appendChild(clone);
   }
@@ -251,48 +278,6 @@ function listarPerros(dog) {
     //Creamos los nodos de perros
     let dogNode = crearNodo("option", null, null, [], [{ 'value': dog }]);
     dogNode.appendChild(document.createTextNode(dog));
-    document.getElementById('dogs').appendChild(dogNode);
+    document.getElementById('doglist').appendChild(dogNode);
   }
 }
-
-/*
-//!Demo de DogAPI
-//https://dog.ceo/dog-api/breeds-list
-function getDog() {
-  var selectedDog = $('.dog-selector option:selected').val();
-  dogURL = selectedDog.replace(/-/g, '/');
-  $.getJSON('https://dog.ceo/api/breed/' + dogURL + '/images/random', function (result) {
-    $('.demo-image').html('<img src=\'' + result.message + '\'>');
-  });
-}
-function loadDogs() {
-  $.getJSON('https://dog.ceo/api/breeds/list/all', function (result) {
-    var breeds = result.message;
-    firstDog = Object.keys(breeds)[0];
-    $.each(breeds, function (dog, breed) {
-      if (breeds[dog].length >= 1) {
-        for (i = 0; i < breeds[dog].length; i++) {
-          $('.dog-selector').append('<option value="' + dog + '-' + breeds[dog][i] + '">' + breeds[dog][i] + ' ' + dog + '</option>');
-        }
-      }
-      else if (breeds[dog].length < 1) {
-        $('.dog-selector').append('<option value="' + dog + '">' + dog + '</option>');
-      }
-    });
-    $.getJSON('https://dog.ceo/api/breed/' + firstDog + '/images/random', function (result) {
-      $('.demo-image').html('<img src=\'' + result.message + '\'>');
-    });
-  });
-}
-$('.dog-selector').change(function () {
-  $('.dog-selector option:selected').each(function () {
-    getDog();
-  });
-});
-$('.get-dog').click(function () {
-  getDog();
-});
-$(document).ready(function () {
-  loadDogs();
-});
-*/
